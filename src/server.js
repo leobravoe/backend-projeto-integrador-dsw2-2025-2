efe835a7da4ef39882cb21aa1e3fe77d869c6ede
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-08-22 18:57:50 -0300

    ...

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 const app = express();
+
 app.get("/", (req, res) => {
     res.send("olá mundo!!!");
 });
+
 app.get("/html", (req, res) => {
     res.send("<h1>teste</h1><p>Parágrafo</p>");
 });
+
 app.listen(3000, () => 
     console.log(`Servidor rodando em http://localhost:3000`)
 );

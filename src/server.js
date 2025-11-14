937bb24ac328a324d2b5087bea1366f50086fadd
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-11-14 17:09:26 -0300

    Adicionando o Middleware de autenticação. Mas ainda não deixarei ligado.

--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@ import express from "express";
 import cors from "cors";
 import usuariosRoutes from "./routes/usuarios.routes.js";
 import chamadosRoutes from "./routes/chamados.routes.js";
+import { authMiddleware } from "./middlewares/auth.js";
 
 const app = express();
 
 // Configura o back-end em modo permissivo
 app.use(cors());
-
+// Converte os dados enviados em JSON (cria o req.body)
 app.use(express.json());
 
 // configura as rotas de usuário
